0cebdb76ffec335bb1cc4dbd096bab45707f3bca
Ayush Trivedi <ayushtrivedi2955@gmail.com>
2023-01-18 23:30:58 +0530

    minor change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     const date=new Date();
     const year=date.getFullYear();
-    return <footer><p>Copyright {year}</p></footer>;
+    return <footer><p>© {year} Ayush Trivedi</p></footer>;
 }
 
 export default Footer;
